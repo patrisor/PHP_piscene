8001812e2f72f7321bf164fef958b942160f5c73
patrisor <p.sorys.at42@gmail.com>
2019-09-16 21:12:28 -0700

    updated d05 bugs

--- d05/ex11/ex11.sql
+++ d05/ex11/ex11.sql
@@ -1,1 +1,1 @@
-SELECT UPPER(`last_name`) AS `NAME`, `first_name`, `price` FROM `member` JOIN `user_card` ON member.id_user_card = user_card.id_user JOIN `subscription` ON member.id_sub = subscription.id_sub WHERE `price` > 42 ORDER BY `last_name` ASC, `first_name` ASC;
+SELECT UPPER(user_card.last_name) AS 'NAME', first_name, price FROM member INNER JOIN user_card ON member.id_user_card = user_card.id_user INNER JOIN subscription ON member.id_sub = subscription.id_sub WHERE price > 42 ORDER BY user_card.last_name, first_name;
