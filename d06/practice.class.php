68c083a9f52f99824f2c53b86bc680abd4c6de16
patrisor <p.sorys.at42@gmail.com>
2019-09-18 23:35:29 -0700

    added d06 stuff

---
+++ d06/practice.class.php
@@ -0,0 +1,32 @@
+<?php
+Class Color 
+{
+	const CST1 = 42;
+
+	public function __construct(array $kwargs)
+	{
+		// if (array_key_exists('arg1', $kwargs))
+		// $kwargs['arg1']
+		return ;
+	}
+
+	// public function __get()
+	
+	// public function __set()
+	
+	// public function __toString()
+
+	// This is used when the class is passed as a function
+	// public function __invoke()
+
+	// Copies the instance to a new memory area
+	// $instance2 = clone $color;
+	// public function __clone()
+
+	public function __destruct()
+	{
+		return ;
+	}
+
+}
+?>
